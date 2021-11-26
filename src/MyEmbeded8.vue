ed5e3db736a46aed9ce8c0ff45a1b8dadc998259
Anton Porshnev <paldraken@gmail.com>
2021-11-26 18:08:43 +0300

    init

--- src/MyEmbeded8.vue
+++ src/MyEmbeded8.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome Embeded App"/>
   </div>
 </template>
 
@@ -9,7 +8,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'MyEmbeded8',
   components: {
     HelloWorld
   }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import MyEmbeded8 from './MyEmbeded8.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(MyEmbeded8),
 }).$mount('#app')
